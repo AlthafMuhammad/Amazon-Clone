ff2786bc517eb8e4a4a8f9b1ca090bd0b4395ffe
*Althaf <althafmuhammad@computer.org>
2020-07-22 16:51:49 +0530

    Updates

---
+++ precache-manifest.f101b6d8f61b9b158825adafeeb53d3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08b7bdb7059d04cdb131cae3c26ad55",
+    "url": "/Amazon-Clone/index.html"
+  },
+  {
+    "revision": "9ef3dc930c1bfedad8a8",
+    "url": "/Amazon-Clone/static/css/main.1b24981d.chunk.css"
+  },
+  {
+    "revision": "6c486a434c96f4f13185",
+    "url": "/Amazon-Clone/static/js/2.ea6f2308.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/Amazon-Clone/static/js/2.ea6f2308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ef3dc930c1bfedad8a8",
+    "url": "/Amazon-Clone/static/js/main.df721276.chunk.js"
+  },
+  {
+    "revision": "4cb599ab17126a081400",
+    "url": "/Amazon-Clone/static/js/runtime-main.300eae63.js"
+  }
+]);
